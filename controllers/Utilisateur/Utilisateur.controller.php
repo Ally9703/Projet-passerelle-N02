da9058b1157b738a9708d8ed01240d5159651b95
alliance tshinadyi <allytshindayi@gamil.com>
2023-07-27 17:44:37 +0100

    vérication du compte activé ou pas dans le mail

--- controllers/Utilisateur/Utilisateur.controller.php
+++ controllers/Utilisateur/Utilisateur.controller.php
@@ -1,20 +1,40 @@
 <?php
 
 require_once ("./controllers/MainController.controller.php");
-require_once ("./models/Visiteur/Visiteur.controller.php");
+require_once ("./models/Utilisateur/Utilisateur.model.php");
 
 // Classe VisiteurController  pour la gestion des visiteurs 
 class UtilisateurController extends MainController{
 
-    // private $visiteurManager;
+// Model
+    private $utilisateurManager;
 
-    // public function __construct(){
-    //     $this->visiteurManager = new VisiteurManager();
-    // }
+    public function __construct(){
+        $this->utilisateurManager = new UtilisateurManager();
+    }
 
     public function validation_login($login, $password){
 
-        echo "test";
+        // Vérifier si le compte est activé
+        if($this->utilisateurManager->isCombinaisonValide($login, $password)){
+            if ($this->utilisateurManager->estCompteActive($login)) {
+                Toolbox::ajouterMessageAlerte("Bon retour sur le site" .$login. "!", Toolbox::COULEUR_VERTE);
+                $_SESSION['profil'] = [
+                    "login" => $login
+                ];
+
+                // redirection vers le compte du profil
+                header("Location :" .URL."compte/profil");
+            }else{
+                Toolbox::ajouterMessageAlerte("Le compte ".$login." n'a pas été activé par mail ",Toolbox::COULEUR_ROUGE);
+
+                // Renvoyer le mail de validation
+                header("Location : ".URL."login");
+            }
+        }else{
+            Toolbox::ajouterMessageAlerte("Email ou le mot de passe non valide",Toolbox::COULEUR_ROUGE);
+            header("Location : ".URL."login");
+        }
 
     }
 
